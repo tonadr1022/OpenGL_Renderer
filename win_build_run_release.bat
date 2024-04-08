387e3ae0249940407eee8d484bbd5574c06b8ab4
Tony Adriansen <tony.adriansen@gmail.com>
2024-04-07 23:09:32 -0500

    untested bat multicore build

--- win_build_run_release.bat
+++ win_build_run_release.bat
@@ -4,11 +4,11 @@ if not exist build-release-win (
 
 cd build-release-win || exit /b
 
-if not exist CMakeCache.txt (
-    cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release ..
-)
+rem Get the number of cores from the environment variable
+set "NUM_CORES=%NUMBER_OF_PROCESSORS%"
 
-cmake --build . --target opengl_renderer --config Release
+cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --target opengl_renderer --config Release -- /maxcpucount:%NUM_CORES%
 cd ..
 
 .\build-release-win\Release\opengl_renderer.exe
