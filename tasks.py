cd50314ea1b075d62902ed699af17d01834e1cf3
Tony Adriansen <tony.adriansen@gmail.com>
2024-04-15 17:54:18 -0500

    windows compat tasks.py

--- tasks.py
+++ tasks.py
@@ -1,10 +1,11 @@
 import os
 import subprocess
 import argparse
+import platform
 
-build_dir = "build-debug"
-project_name = "opengl_renderer"
-executable_name = "opengl_renderer"
+BUILD_DIR = "build-debug"
+PROJECT_NAME = "opengl_renderer"
+EXECUTABLE_NAME = "opengl_renderer"
 
 
 def cmake():
@@ -20,7 +21,11 @@ def cmake():
 
 def build():
     print("Building")
-    cmake_build_command = ["cmake", "--build", ".", "--", "-j", str(os.cpu_count())]
+    if platform.system() == "Windows":
+        parallel_flag = "/m:"
+    else:
+        parallel_flag = "-j"
+    cmake_build_command = ["cmake", "--build", ".", "--", f"{parallel_flag}{str(os.cpu_count())}"]
     build_process = subprocess.Popen(cmake_build_command)
     build_process.wait()
 
@@ -29,9 +34,22 @@ def build():
         exit(1)
 
 
-def run():
-    execute_command = [os.path.join(build_dir, executable_name)]
-    execute_process = subprocess.Popen(execute_command)
+def _find_executable(directory, executable_name):
+    for root, dirs, files in os.walk(directory):
+        if executable_name in files:
+            return os.path.join(root, executable_name)
+    return None
+
+
+def run(executable_name):
+    if platform.system() == "Windows":
+        executable_name += ".exe"
+
+    executable_path = _find_executable(BUILD_DIR, executable_name)
+    if not executable_path:
+        print("Error finding executable, exiting")
+        exit(1)
+    execute_process = subprocess.Popen(executable_path)
     execute_process.wait()
 
     if execute_process.returncode != 0:
@@ -40,20 +58,22 @@ def run():
 
 
 def build_run():
-    os.makedirs(build_dir, exist_ok=True)
-    os.chdir(build_dir)
+    comp_name = "compile_commands.json"
+    os.makedirs(BUILD_DIR, exist_ok=True)
+    os.chdir(BUILD_DIR)
     cmake()
     build()
     os.chdir("..")
-    if os.path.exists("compile_commands.json"):
-        os.remove("compile_commands.json")
+    if os.path.exists(comp_name) or os.path.islink(comp_name):
+        print("Remove test")
+        os.remove(comp_name)
     os.symlink(
-        os.path.join(build_dir, "compile_commands.json"), "compile_commands.json"
+        os.path.join(BUILD_DIR, comp_name), comp_name
     )
-    run()
+    run(EXECUTABLE_NAME)
 
 
-parser = argparse.ArgumentParser(description=f"{project_name} build tools")
+parser = argparse.ArgumentParser(description=f"{PROJECT_NAME} build tools")
 
 if __name__ == "__main__":
     build_run()
